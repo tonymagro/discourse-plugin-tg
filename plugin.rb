45fbd3ed7334bc45c614672c0be0d3676f5e3717
Tony Magro <tony@twogear.com>
2022-04-26 23:56:35 -0500

    Update plugins.rb info

--- plugin.rb
+++ plugin.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
-# name: discourse-plugin-name
+# name: discourse-plugin-tg
 # about: TODO
 # version: 0.0.1
-# authors: Discourse
-# url: TODO
+# authors: TG
+# url: https://github.com/tonymagro/discourse-plugin-tg
 # required_version: 2.7.0
 # transpile_js: true
 
-enabled_site_setting :plugin_name_enabled
+enabled_site_setting :tg_enabled
 
 after_initialize do
 end
