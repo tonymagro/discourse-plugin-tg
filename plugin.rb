8c6cd6944a4caf74ce2cb4ef397ef3035f8b796f
Tony Magro <tony@twogear.com>
2023-02-03 15:46:58 -0600

    Minimal plugin

--- plugin.rb
+++ plugin.rb
@@ -9,24 +9,26 @@
 
 enabled_site_setting :tg_enabled
 
+PLUGIN_NAME = "tg".freeze
+
 after_initialize do
   # Listen to topic view events
-  DiscourseEvent.on(:topic_viewed) do |topic_view, opts|
+  #DiscourseEvent.on(:topic_viewed) do |topic_view, opts|
     # Retrieve all posts in the thread
-    posts = topic_view.posts
+    #posts = topic_view.posts
 
     # Initialize an array to store all YouTube links
-    youtube_links = []
+    #youtube_links = []
 
     # Loop through each post and extract its YouTube links
-    posts.each do |post|
-      # Find all YouTube links in the post
-      post_links = post.raw.scan(/https:\/\/(www\.)?youtube.com\/watch\?v=[a-zA-Z0-9]+/)
-      youtube_links += post_links unless post_links.empty?
-    end
+    # posts.each do |post|
+    #   # Find all YouTube links in the post
+    #   post_links = post.raw.scan(/https:\/\/(www\.)?youtube.com\/watch\?v=[a-zA-Z0-9]+/)
+    #   youtube_links += post_links unless post_links.empty?
+    # end
 
-    # Store the unique links in the topic view
-    topic_view.add_bottom_description_panel("youtube-links", { links: youtube_links.uniq })
-  end
+    # # Store the unique links in the topic view
+    # topic_view.add_bottom_description_panel("youtube-links", { links: youtube_links.uniq })
+  #end
 end
 
